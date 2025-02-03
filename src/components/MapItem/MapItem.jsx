94596f506edc08dba224b46407acf401ebaacc48
sans1998 <hero887483214@gmail.com>
2025-02-04 02:00:54 +0800

    feat:新增地圖地標TIP

--- src/components/MapItem/MapItem.jsx
+++ src/components/MapItem/MapItem.jsx
@@ -2,28 +2,52 @@ import styles from "./map.module.css";
 import { Row, Col } from "antd";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; // 引入 Leaflet 的樣式
+import landmarks from "../../json/place.json";
+import { HeartFilled, HeartOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { useState } from "react";
 
 export default function MapItem() {
+  const center = [36.23541690015412, 137.97220383903155];
+  const [click, setClick] = useState(false);
+  const ButtonClick = () => {
+    setClick(!click);
+  };
   return (
     <div className={styles.fullScreen}>
-      {/* 地圖區域 */}
       <Row className={styles.mapContainer}>
         <Col span={24} className={styles.mapCol}>
-          <MapContainer
-            center={[25.033, 121.5654]}
-            zoom={13}
-            className={styles.map}
-          >
+          <MapContainer center={center} zoom={7} className={styles.map}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; 
-                            <a href="https://www.openstreetmap.org/copyright">
-                                OpenStreetMap
+              attribution='&copy;
+                            <a href="https://www.openstreetmap.org/copyright">OpenStreetMap
                             </a> contributors'
             />
-            <Marker position={[25.033, 121.5654]}>
-              <Popup>這是台北 101 的位置。</Popup>
+            <Marker position={center}>
+              <Popup>地圖中心</Popup>
             </Marker>
+            {/* 從 landmarks 中匯入的 Marker */}
+            {landmarks.map((landmark) => (
+              <Marker key={landmark.id} position={landmark.position}>
+                <Popup className={styles.popup}>
+                  <div className={styles.landmarkName}>{landmark.name}</div>
+                  <img src={landmark.image} className={styles.img} />
+                  <div className={styles.buttonContainer}>
+                    <div
+                      className={styles.buttonHeart}
+                      onClick={(e) => {
+                        e.stopPropagation(); // 阻止點擊事件冒泡
+                        ButtonClick();
+                      }}
+                    >
+                      {click === false ? <HeartOutlined /> : <HeartFilled />}
+                    </div>
+                    <Button type="primary">加入行程</Button>
+                  </div>
+                </Popup>
+              </Marker>
+            ))}
           </MapContainer>
         </Col>
       </Row>
