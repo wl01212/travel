43ca8d5cd3e9fe7fa53c2ef1f6e2508c1e014f3c
sans1998 <hero887483214@gmail.com>
2025-02-07 01:37:57 +0800

    chore:新增圖片與修正樣式

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import App from './App.jsx'
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
 import store from "./store";
+import "/src/index.css" ;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
