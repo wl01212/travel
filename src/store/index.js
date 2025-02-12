df69e4ec3b0051c62b99dfe3fcade0cd3aa3a697
sans1998 <hero887483214@gmail.com>
2025-02-13 02:00:45 +0800

    feat:新增attraction狀態管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
-
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter";
 import favoriteSlice from "./favorite";
-import scheduleReducer from './schedule';
+import scheduleReducer from "./schedule";
+import addScheduleItems from "./attraction";
 
 const store = configureStore({
-  reducer: { counter: counterReducer, favorite: favoriteSlice,schedule: scheduleReducer, }
+  reducer: {
+    counter: counterReducer,
+    favorite: favoriteSlice,
+    schedule: scheduleReducer,
+    attraction: addScheduleItems,
+  },
 });
 
 export default store;
